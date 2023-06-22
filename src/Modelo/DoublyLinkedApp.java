5425249f237d6b6a293c37b018aaa3d72aa4868c
Bidob <mauriceuve3221@gmail.com>
2023-06-21 22:04:40 -0500

    Codigo con pares invertidos

--- src/Modelo/DoublyLinkedApp.java
+++ src/Modelo/DoublyLinkedApp.java
@@ -52,6 +52,7 @@ class DoublyLinkedApp {
                     theList.displayForward();
                     theList.invertir();
                     theList.displayForward();
+                    theList.invertir();
                 }
 
                 case 3 -> {
@@ -65,27 +66,5 @@ class DoublyLinkedApp {
 
             System.out.println();
         }
-        /*
-        theList.insertFirst(22);
-        theList.insertFirst(44);
-        theList.insertFirst(66);
-
-        theList.insertLast(11);
-        theList.insertLast(33);
-        theList.insertLast(55);
-
-        theList.displayForward();
-        theList.displayBackward();
-
-        theList.deleteFirst();
-        theList.deleteLast();
-        theList.deleteKey(11);
-
-        theList.displayForward();
-
-        theList.insertAfter(22, 77);
-        theList.insertAfter(33, 88);
-
-        theList.displayForward();*/
     }
 }
